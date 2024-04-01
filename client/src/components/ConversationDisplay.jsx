4a8760522613197944f01e5eedd9facb1a63e0b4
Shhady Serhan <100072103+shhady@users.noreply.github.com>
2024-04-01 14:08:33 +0300

    Update ConversationDisplay.jsx

--- client/src/components/ConversationDisplay.jsx
+++ client/src/components/ConversationDisplay.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export const ConversationDisplay = ({ chat }) => {
   return (
     <Link
-      to={`/chat-page/?sender=${chat.sender._id}&receiver=${chat.receiver._id}&hub=${chat.chatHub}`}
+      to={`/chat-page/?sender=${chat?.sender._id}&receiver=${chat?.receiver._id}&hub=${chat?.chatHub}`}
       style={{ textDecoration: "none", color: "black" }}
     >
       <DisplayChatsStyle>
